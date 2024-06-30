b22d50fdf0684ee4b69e4094e3bbe231a94e975b
Stakshi Pandey <80407850+carr-o-t@users.noreply.github.com>
2024-06-30 18:43:50 +0530

    styles: fix login page css

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,7 +3,7 @@ import { LoginForm } from "../components/auth/login-form";
 
 const LoginPage: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen p-4">
       <LoginForm />
     </div>
   );
